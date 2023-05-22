f2699c380cd8c2fa2e4b9992a5bf06ec09b8b4c4
김덕용 <yh100112@naver.com>
2023-05-22 18:19:40 +0900

    unordered map 복습

--- unordered_map/unordered_map.cpp
+++ unordered_map/unordered_map.cpp
@@ -4,23 +4,26 @@
 using namespace std;
 
 int main(){
-
   unordered_map<string,int> um;
 
   if(um.empty()){
     printf("unordered_map은 비어있다.\n");
   }
 
-  um.insert(make_pair("key",1));
-  um["banana"] = 2;
+  um.insert(unordered_map<string,int>::value_type({"key",1}));
   um.insert({"melon",3});
+  um.insert({"apple",5});
 
-  printf("unordered_map의 크기 : %d\n",um.size());
+  printf("unordered_map의 크기 : %lu\n",um.size());
 
   for(auto elem : um){
     cout << "key : " << elem.first << " value : " << elem.second << endl;
   }
 
-  printf("%d\n",um.find("banana")->second);
-  printf("%d\n",um.begin()->second);
+  unordered_map<int, int> um2;
+  um2[3]++;            //map과 마찬가지로 그냥 um2[] 이렇게만 하면 알아서 키와 쌍을 이루는 value를 넣어줌 (int이므로 0을 넣어줌)
+  printf("%d\n",um2[3]); // 1
 }
+/*
+ * unordered_map은 key를 정렬해주지 않는다. ( hash table로 구현되어 있기 때문 )
+ */
