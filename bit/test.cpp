e1ba371e1218fc5a4a56cdc21b4557b047d22a85
김덕용 <yh100112@zempot.com>
2023-06-26 15:32:40 +0900

    aaa

--- bit/test.cpp
+++ bit/test.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main() {
-  int64_t a = 9288674231451648;
-  a |= (int64_t)36387350320427;
-
+  int16_t a = 0;
+  a |= 1 << 1;
+  cout << a << endl; // 2 : 0010
+  a |= 1;
+  cout << a << endl; // 3 : 0011
+  a << 2;
   cout << a << endl;
 }
