528556401c77c52e0037ec5971b4bf15a98e7262
yh100112 <yh100112@naver.com>
2023-07-23 18:03:24 +0900

    c++ string erase 추가

--- string/erase.cpp
+++ string/erase.cpp
@@ -5,4 +5,11 @@ int main(){
     string a = "applebanana";
     a.erase(a.find("ba"),2);
     cout << a << endl; // "applenana"
+
+    string b = "abcde";
+    b.erase(b.begin());
+    cout << b << endl; // bcde    
+
+    b.erase(0);
+    cout << b << endl; // ""
 }
