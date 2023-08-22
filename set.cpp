60439e577bd904b2df10241af31d47883a31e571
김덕용 <yh100112@naver.com>
2023-08-22 11:58:49 +0900

    c++ set iterator를 통해 원소 접근

--- set.cpp
+++ set.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int main() {
-  set<int16_t> s;
-  s.insert(10);
-  s.insert(10);
-  s.insert(30);
-  s.insert(20);
+    set<int16_t> s;
+    s.insert(10);
+    s.insert(10);
+    s.insert(30);
+    s.insert(20);
 
-  for(auto& v: s)
-    cout << v << endl; // 10 20 30
+    for(auto& v: s)
+        cout << v << endl; // 10 20 30
 
-  if (s.find(10) != s.end())
-    cout << *s.find(10) << endl; // 10
+    if (s.find(10) != s.end())
+        cout << *s.find(10) << endl; // 10
+    
+    cout << *s.begin() << endl; // 10
+    s.erase(s.begin());         // 10을 지움
+    cout << *s.begin() << endl; // 20
 }
