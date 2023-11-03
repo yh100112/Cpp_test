0bc7c313d06c8eb8f9423b13034251b0c7dbc158
김덕용 <yh100112@naver.com>
2023-11-03 17:11:36 +0900

    c++ static instance 공부

--- static/config.cpp
+++ static/config.cpp
@@ -2,29 +2,31 @@
 #include <stdio.h>
 using namespace std;
 
-
 class config
 {
 public:
-  static config& instance() {
-    static config obj; 
-    printf("%p\n", &obj); 
-    return obj;
-  }
+  static config& instance();
 
   void incr() {
     ++cnt;
   }
 
   int16_t cnt = 0;
-
 };
 
+config& config::instance()
+{
+    static config obj;
+    return obj;
+}
+
 int main() {
-  cout << config::instance().cnt << endl;
+  cout << config::instance().cnt << endl; // 0
   config::instance().incr();
-  cout << config::instance().cnt << endl;
+
+  cout << config::instance().cnt << endl; // 1
   config::instance().incr();
   config::instance().incr();
-  cout << config::instance().cnt << endl;
+
+  cout << config::instance().cnt << endl; // 3
 }
