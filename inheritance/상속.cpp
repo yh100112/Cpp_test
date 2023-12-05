fa89f9f2adbaf372e152bac338522efed2ec862c
김덕용 <yh100112@naver.com>
2023-12-05 11:28:35 +0900

    상속 복습

---
+++ inheritance/상속.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Person{
+private:
+    string name;
+public:
+    Person(string name){
+        this->name = name;
+    }
+    string getName(){ return name; }
+    void showName(){ cout << "이름 : " <<getName() << '\n'; }
+};
+
+class Student : private Person{ 
+private:
+    int studentID;
+public:
+    Student(int studentID, string name) : Person(name) {
+        this->studentID = studentID;
+    }
+
+    void show(){
+        cout << "-학생번호 : " << studentID << '\n';
+        cout << "-학생이름 : " << getName() << '\n'; 
+    }
+
+    //오버라이딩
+    void showName(){
+        cout << "학생 이름 : " << getName() << '\n';
+    }
+
+};
+
+int main(){
+    Student student(1,"김덕용");
+    student.show();
+    student.showName();
+    return 0;
+}
