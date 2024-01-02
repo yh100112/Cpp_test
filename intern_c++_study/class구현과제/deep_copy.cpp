f80afb525f8e5236f079c88daa7e286f14aaf6fc
김덕용 <yh100112@naver.com>
2024-01-02 10:36:34 +0900

    복사생성자, 대입연산자 복습 및 추가

--- intern_c++_study/class구현과제/deep_copy.cpp
+++ intern_c++_study/class구현과제/deep_copy.cpp
@@ -5,31 +5,30 @@ using namespace std;
 class A{
 private:
   char* name;
-  int age;
 public:
-  A(const char* myname, int myage): age(myage){
-    name = new char[strlen(myname) + 1];
-    strcpy(name,myname);
-  }
+    A(const char* myname){
+        name = new char[strlen(myname) + 1];
+        strcpy(name,myname);
+    }
 
-  A(const A& copy): age(copy.age){
-    name = new char[strlen(copy.name) + 1];
-    strcpy(name,copy.name);
-  }
+    // 복사생성자 직접 생성
+    A(const A& copy){
+        name = new char[strlen(copy.name) + 1];
+        strcpy(name,copy.name);
+    }
 
-  void showInfo() const{
-    cout << "나이 : " << age << endl;
-  }
+    void showInfo() const{
+        cout << "이름 : " << name << endl;
+    }
 
-  ~A(){
-    delete []name;
-    cout << "소멸자 호출" << endl;
-  }
-
+    ~A(){
+        delete []name;
+        cout << "소멸자 호출" << endl;
+    }
 };
 
 int main(){
-  A a("이름",20);
+  A a("deokyong");
   A b = a;
   a.showInfo();
   b.showInfo();
--- intern_c++_study/class구현과제/shallow_copy.cpp
+++ intern_c++_study/class구현과제/shallow_copy.cpp
@@ -14,8 +14,7 @@ public:
   }
 
   void ShowInfo() const{
-    cout << "이름 : " << name << endl;
-    cout << "나이 : " << age << endl;
+    cout << name << ' ' << age << endl;
   }
 
   ~A(){
@@ -25,7 +24,7 @@ public:
 };
 
 int main(){
-  A a("이름",20);
+  A a("덕용",20);
   A b = a; // shallow copy로 주소값만 복사해옴 ( 디폴트 복사생성자 )
   a.ShowInfo();
   b.ShowInfo(); //객체 a의 name을 가리키는 주소 값을 b도 같이 할당받아, 두 객체가 같은 문자열을 delete해줘서 에러남
