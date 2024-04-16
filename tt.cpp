43696ece43ad3974e047298f3366c8f637b08b86
김덕용 <yh100112@naver.com>
2024-04-16 15:14:40 +0900

    boj - 2630 색종이 만들기

--- tt.cpp
+++ tt.cpp
@@ -1,19 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-int t, n;
+int n, a[130][130], ret[2];
+
+bool check(int y, int x, int n) {
+  for (int i = y; i < y + n; i++)
+    for (int j = x; j < x + n; j++)
+      if (a[i][j] != a[y][x]) return false;
+  return true;
+}
+
+void go(int y, int x, int n) {
+  if (check(y, x, n)) {
+    ret[a[y][x]]++;
+    return;
+  }
+
+  for (int i = 0; i < 2; i++)
+    for (int j = 0; j < 2; j++)
+      go(y + i * (n / 2), x + j * (n / 2), n / 2);
+}
 
 int main() {
-  cin >> t;
-  while(t--) {
-    cin >> n;
-    int a[n];
-    for (int i = 0; i < n; i++)
-      cin >> a[i];
-    
-    sort(a, a + n);
-    int mx = INT_MIN;
-    for (int i = 0; i < n - 2; i++)
-      mx = max(mx, abs(a[i] - a[i + 2]));
-    cout << mx << "\n";
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> a[i][j];
+    }
   }
+
+  go(0, 0, n);
+  cout << ret[0] << "\n" << ret[1] << "\n";
 }
